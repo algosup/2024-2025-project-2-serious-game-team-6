4626e4191c373e483b47fdf8524e4e21cbe26c1c
MichelRiff <michel.riff@algosup.com>
2024-11-29 18:30:13 +0100

    Interaction Rework
    Copied the same Interaction system for the 3D, adapted for the 2D by ensuring no conflicts between both

---
+++ Game/3d-game-project/2D/Interaction 2D/Interaction/playerInteractor2d.gd
@@ -0,0 +1,33 @@
+extends Interactor2D
+
+@export var player: CharacterBody2D # Reference to the player
+
+# Track the closest interactable and currently held object
+var cached_closest: Interactable2D
+
+func _ready() -> void:
+	controller = player
+
+# Update the closest interactable object in each physics frame
+func _physics_process(delta: float) -> void:
+	var new_closest: Interactable2D = get_closest_interactable()
+	if new_closest != cached_closest:
+		if is_instance_valid(cached_closest):
+			unfocus(cached_closest)
+		if new_closest:
+			focus(new_closest)
+		cached_closest = new_closest
+
+# Handle area exit events
+func _on_area_exited(area: Interactable2D) -> void:
+	if cached_closest == area:
+		unfocus(area)
+
+# Handle interaction inputs
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact"):
+		if cached_closest.get_parent() is Teleporter2D:
+			interact_with_teleporter(cached_closest.get_parent())
+
+func interact_with_teleporter(teleporter: Teleporter2D) -> void:
+	teleporter._on_interactable_2d_interacted(self)
