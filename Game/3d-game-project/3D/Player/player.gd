444548537495c808128510d2f56c2dcc52f1fe93
EnzoGuillouche <145991192+EnzoGuillouche@users.noreply.github.com>
2024-12-16 14:45:05 +0100

    Fixed export issues

--- Game/3d-game-project/3D/Player/player.gd
+++ Game/3d-game-project/3D/Player/player.gd
@@ -17,7 +17,7 @@ func _ready():
 	sprite.play()
 	footstep.play()
 	footstep.stream_paused = true  # Ensure sound is paused at the start
-	DebugMenu.style = DebugMenu.Style.VISIBLE_DETAILED
+	# DebugMenu.style = DebugMenu.Style.VISIBLE_DETAILED
 
 func _physics_process(delta): # control
 	if movement_disabled:
